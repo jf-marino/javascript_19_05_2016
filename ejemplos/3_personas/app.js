653f0f8311679e652a2d19a6ed3862d61a934edd
Juan Francisco Marino <marinojuanfrancisco@gmail.com>
2016-05-19 02:10:36 -0300

    Agregados 404

--- ejemplos/3_personas/app.js
+++ ejemplos/3_personas/app.js
@@ -10,7 +10,7 @@ server.use(restify.fullResponse());
 
 
 /**
- * Rutas
+ * Rutas de usuarios
  */
 
 // Mostrar todos
@@ -28,20 +28,56 @@ server.get('/users/:username', function(req, res, next) {
 
 // Agregar uno  (usar req.body para obtener los datos)
 server.post('/users/new', function(req, res, next) {
-    
+    res.send(404);
     next();
 });
 
-// Cambiar el password de
+// Cambiar el password de un usuario
 server.put('/users/:username/change', function(req, res, next) {
-    
+    res.send(404);
     next();
 });
 
+// Borrar un usuario
 server.del('/users/:username/delete', function(req, res, next) {
-    
+    res.send(404);
     next();
 });
+
+
+/**
+ *  Rutas de persona
+ */
+// Mostrar todas las personas
+server.get('/people', function(req, res, next) {
+    res.send(404);
+    next();
+});
+
+// Mostrar una persona en particular
+server.get('/people/:name', function(req, res, next) {
+    res.send(404);
+    next();
+});
+
+// Agregar una persona
+server.post('/people/new', function(req, res, next) {
+    res.send(404);
+    next();
+});
+
+// Cambiar los datos de una persona
+server.put('/people/:name/change', function(req, res, next) {
+    res.send(404);
+    next();
+});
+
+// Borrar a una persona
+server.del('/people/:name/delete', function(req, res, next) {
+    res.send(404);
+    next();
+});
+
 
 
 server.listen(8080, 'localhost', function() {
